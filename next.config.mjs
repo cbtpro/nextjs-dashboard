2ed29a11224a72ba7934afe497e1bb4f4f9bce3c
chenbitao <cbtpro@163.com>
2024-09-02 19:16:10 +0800

    feat: 启用实验特性部分预渲染 ppr

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    // 实现部分预呈现
+    ppr: 'incremental',
+  },
+};
 
 export default nextConfig;
